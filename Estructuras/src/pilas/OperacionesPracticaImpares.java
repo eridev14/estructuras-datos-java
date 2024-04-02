624741cf94db6516be8e8965423b857ba1477c7b
eridev14 <eridev14hc@gmail.com>
2024-04-02 16:56:49 -0400

    add function sumarPilas

--- Estructuras/src/pilas/OperacionesPracticaImpares.java
+++ Estructuras/src/pilas/OperacionesPracticaImpares.java
@@ -4,6 +4,8 @@
  */
 package pilas;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author ERIK HUAYLLAS COAQUIRA PRG300
@@ -237,15 +239,46 @@ public class OperacionesPracticaImpares {
     /**
      * Ejer 17
      *
-     * Dada una P sumar los elementos primos de la pila
+     * Dada dos pila A,B, sumar sus elementos de ambas pilas, considere que las
+     * pilas deben ser del mismo tamaño.
      *
+     * @param A
+     * @param B
      */
+    public void sumarPilas(Pila A, Pila B, Pila C) {
+        Pila auxA = new Pila(A.max);
+        Pila auxB = new Pila(B.max);
+        Pila auxC = new Pila(C.max);
+
+        if (B.tope != A.tope) {
+            JOptionPane.showMessageDialog(null, "la Pila A y B no son del mismo tama;o");
+            return;
+        }
+
+        while (!A.estaVacio()) {
+            int valA = A.pop();
+            auxA.push(valA);
+            int valB = B.pop();
+            auxB.push(valB);
+            int sum = valA + valB;
+            auxC.push(sum);
+        }
+
+        A.reset(auxA);
+        B.reset(auxB);
+        C.reset(auxC);
+    }
+
     /**
      * Ejer 19
      *
-     * Dada una P sumar los elementos primos de la pila
+     * Dada dos Pilas A,B, realizar la Unión de elementos en una Pila C (la
+     * unión debe ser como en teoría de conjuntos)
      *
      */
+    
+    
+    
     /**
      * Ejer 21
      *
@@ -296,15 +329,25 @@ public class OperacionesPracticaImpares {
      */
     public static void main(String[] args) {
         OperacionesPracticaImpares op = new OperacionesPracticaImpares();
-        Pila p = new Pila(10);
-        p.push(1);
-        p.push(2);
-        p.push(3);
-        p.push(4);
-        p.push(5);
-        System.out.println(" " + p.listar() + "\n\n");
-        op.insertarX(p, 1);
-        System.out.println(" " + p.listar() + "\n\n");
+        Pila p1 = new Pila(10);
+        Pila p2 = new Pila(10);
+        Pila p3 = new Pila(p1.max);
+
+        p1.push(1);
+        p1.push(2);
+        p1.push(3);
+        p1.push(4);
+        p1.push(5);
+
+        p2.push(5);
+        p2.push(4);
+        p2.push(3);
+        p2.push(5);
+
+        System.out.println(" " + p1.listar() + "\n\n");
+        System.out.println(" " + p2.listar() + "\n\n");
+        op.sumarPilas(p1, p2, p3);
+        System.out.println(" " + p3.listar() + "\n\n");
 
     }
 }

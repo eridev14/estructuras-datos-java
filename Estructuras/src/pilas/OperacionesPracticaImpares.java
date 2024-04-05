43c9d34d3e4e1cef325b05ff7e224c0d89477887
eridev14 <eridev14hc@gmail.com>
2024-04-05 16:49:07 -0400

    add functions imp

--- Estructuras/src/pilas/OperacionesPracticaImpares.java
+++ Estructuras/src/pilas/OperacionesPracticaImpares.java
@@ -306,70 +306,189 @@ public class OperacionesPracticaImpares {
      * Mediante Pilas realizar la suma y multiplicación de polinomios
      *
      */
-    
+    public void opPilinomiosPilas() {
+
+    }
+
     /**
      * Ejer 23
      *
-     * Dada una P sumar los elementos primos de la pila
+     * Dada una pila P, intercambiar el primer elemento de la pila y el último
+     * elemento de la pila
      *
+     * @param p
      */
+    public void intercambio(Pila p) {
+        Pila aux = new Pila(p.max);
+        Pila aux2 = new Pila(p.max);
+
+        int first = p.pop();
+        while (!p.estaVacio()) {
+            int val = p.pop();
+            aux.push(val);
+        }
+        p.push(first);
+        aux2.reset(aux);
+        p.reset(aux2);
+    }
+
     /**
      * Ejer 25
      *
-     * Dada una P sumar los elementos primos de la pila
+     * Dada una Pila P, eliminar el primer Impar de la Pila.
      *
+     * @param p
      */
+    public void eliminarPrimerImpar(Pila p) {
+        Pila aux = new Pila(p.max);
+
+        while (!p.estaVacio()) {
+            int val = p.pop();
+            if (val % 2 == 1) {
+                break;
+            } else {
+                aux.push(val);
+            }
+        }
+        p.reset(aux);
+    }
+
     /*
       * Ejer 27
      *
-     * Dada una P sumar los elementos primos de la pila
+     * Dada una Pila P, eliminar el elemento mayor de la pila.
      *
      */
+    public void eliminarMayor(Pila p) {
+        Pila aux = new Pila(p.max);
+        int mayor = p.pop();
+        aux.push(mayor);
+        while (!p.estaVacio()) {
+            int val = p.pop();
+            aux.push(val);
+            if (val > mayor) {
+                mayor = val;
+            }
+        }
+        while (!aux.estaVacio()) {
+            int val = aux.pop();
+            if (val != mayor) {
+                p.push(val);
+            }
+        }
+    }
+
     /**
      * Ejer 29
      *
-     * Dada una P sumar los elementos primos de la pila
+     * Dada una Pila P eliminar el segundo elemento menor de la pila
      *
+     * @param p
      */
+    public void eliminarSegundoMenor(Pila p) {
+        Pila aux = new Pila(p.max);
+        int menor = p.pop();
+        aux.push(menor);
+        while (!p.estaVacio()) {
+            int val = p.pop();
+            if (val < menor) {
+                menor = val;
+            }
+            aux.push(val);
+        }
+
+        p.reset(aux);
+
+        int segundoMenor = p.pop();
+        aux.push(segundoMenor);
+        while (!p.estaVacio()) {
+            int val = p.pop();
+            if (val == menor) {
+                aux.push(val);
+                continue;
+            }
+            if (val < segundoMenor) {
+                segundoMenor = val;
+            }
+            aux.push(val);
+        }
+
+        while (!aux.estaVacio()) {
+            int val = aux.pop();
+            if (val == segundoMenor) {
+                continue;
+            }
+            p.push(val);
+        }
+    }
+
     /**
      * Ejer 31
      *
-     * Dada una P sumar los elementos primos de la pila
+     * En una pila P, Generar n-elementos de la serie Fibonacci.
      *
+     * @return 
      */
+    public Pila serieFibonacci() {
+        int n = Integer.parseInt(JOptionPane.showInputDialog("cantidad de elementos de Fibonacci"));
+        Pila p = new Pila(n);
+        int a = 0;
+        int b = 1;
+        for (int i = 0; i < n; i++) {
+            p.push(a);
+            int c = a + b;
+            b = a;
+            a = c;
+        }
+        return p;
+    }
+
     /*
      * Ejer 33
      *
-     * Dada una P sumar los elementos primos de la pila
+     * Dada una pila P, sumar todos los elementos múltiplos de X
      *
      */
+    public int sumaMultiplosX(Pila p, int x) {
+        Pila aux = new Pila(p.max);
+        int sum = 0;
+        while (!p.estaVacio()) {
+            int val = p.pop();
+            if (val % x == 0) {
+                sum += val;
+            }
+            aux.push(val);
+        }
+        return sum;
+    }
+
     /**
      * Ejer 35
      *
-     * Dada una P sumar los elementos primos de la pila
+     * En una pila almacenar los datos de un libro con los siguientes atributos
+     * (id, titulo, ,autor, editorial, precio) y luego realizar las siguientes
+     * operaciones: a) Adicionar b) Listar c) Buscar un libro por el titulo
      *
      */
+    
     public static void main(String[] args) {
         OperacionesPracticaImpares op = new OperacionesPracticaImpares();
-        Pila p1 = new Pila(10);
+        Pila p1;
         Pila p2 = new Pila(10);
-        Pila p3 = new Pila(p1.max + p2.max);
-
+        //Pila p3 = new Pila(p1.max + p2.max);
+        /*
         p1.push(1);
         p1.push(2);
-        p1.push(3);
+        p1.push(2);
         p1.push(6);
         p1.push(7);
-
+         */
         p2.push(3);
         p2.push(4);
         p2.push(5);
         p2.push(6);
 
+        p1 = op.serieFibonacci();
         System.out.println(" " + p1.listar() + "\n\n");
-        System.out.println(" " + p2.listar() + "\n\n");
-        op.union(p1, p2, p3);
-        System.out.println(" " + p3.listar() + "\n\n");
-
     }
 }

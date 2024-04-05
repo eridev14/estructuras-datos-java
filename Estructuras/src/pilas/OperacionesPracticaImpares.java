c4a137a28f387ce456a248a4b8b1c61f1b65fccb
eridev14 <eridev14hc@gmail.com>
2024-04-05 10:09:07 -0400

    add function union conjuntos

--- Estructuras/src/pilas/OperacionesPracticaImpares.java
+++ Estructuras/src/pilas/OperacionesPracticaImpares.java
@@ -275,16 +275,38 @@ public class OperacionesPracticaImpares {
      * Dada dos Pilas A,B, realizar la Unión de elementos en una Pila C (la
      * unión debe ser como en teoría de conjuntos)
      *
+     * @param A
+     * @param B
+     * @param C
      */
-    
-    
-    
+    public void union(Pila A, Pila B, Pila C) {
+        Pila auxA = new Pila(A.max);
+        Pila auxB = new Pila(B.max);
+        //Pila auxC = new Pila(C.max);
+        while (!A.estaVacio()) {
+            int val = A.pop();
+            auxA.push(val);
+            C.push(val);
+        }
+
+        while (!B.estaVacio()) {
+            int val = B.pop();
+            auxB.push(val);
+            C.push(val);
+        }
+
+        OperacionesPracticaPares op = new OperacionesPracticaPares();
+        op.eliminarRepetidos(C);
+        ordenar(C);
+    }
+
     /**
      * Ejer 21
      *
-     * Dada una P sumar los elementos primos de la pila
+     * Mediante Pilas realizar la suma y multiplicación de polinomios
      *
      */
+    
     /**
      * Ejer 23
      *
@@ -331,22 +353,22 @@ public class OperacionesPracticaImpares {
         OperacionesPracticaImpares op = new OperacionesPracticaImpares();
         Pila p1 = new Pila(10);
         Pila p2 = new Pila(10);
-        Pila p3 = new Pila(p1.max);
+        Pila p3 = new Pila(p1.max + p2.max);
 
         p1.push(1);
         p1.push(2);
         p1.push(3);
-        p1.push(4);
-        p1.push(5);
+        p1.push(6);
+        p1.push(7);
 
-        p2.push(5);
-        p2.push(4);
         p2.push(3);
+        p2.push(4);
         p2.push(5);
+        p2.push(6);
 
         System.out.println(" " + p1.listar() + "\n\n");
         System.out.println(" " + p2.listar() + "\n\n");
-        op.sumarPilas(p1, p2, p3);
+        op.union(p1, p2, p3);
         System.out.println(" " + p3.listar() + "\n\n");
 
     }
